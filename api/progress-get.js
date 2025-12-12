3dd13ef6ecff737a18441dbd023a0e3e61c249cd
GethinLane <laneappsltd@gmail.com>
2025-12-12 19:35:47 +0000

    Update progress-get.js

--- api/progress-get.js
+++ api/progress-get.js
@@ -6,6 +6,7 @@ function setCors(req, res) {
   const origin = req.headers.origin;
   if (ALLOWED_ORIGINS.includes(origin)) res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Max-Age", "86400");
